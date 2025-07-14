dc594248ee56bff9585074074e3d29d09596c652
chococooke * web3 <pro.bhimsen@gmail.com>
2025-07-14 09:37:09 +0530

    patch

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {res.send("hello, world")});
+app.get("/", (req, res) => {console.log(req.headers); res.send("hello, world")});
 
 
 app.listen(PORT, () => console.log("Listening on port", PORT));
